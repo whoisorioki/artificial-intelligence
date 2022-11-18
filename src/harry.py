577918644c782b527b34cbd27020acd704153a47
Adrian Orioki <adrianorioki@gmail.com>
2022-11-18 13:16:24 +0300

    Knowledge

---
+++ src/harry.py
@@ -0,0 +1,16 @@
+from logic import *
+
+rain = Symbol("rain") # It is raining.
+hagrid = Symbol("hagrid") # Harry visited Hagrid.
+dumbledore = Symbol("dumbledore") # Harry visited Dumbledore.
+
+sentence = And(rain, hagrid)
+
+knowledge = And(
+    Implication(Not(rain), hagrid),
+    Or(hagrid, dumbledore),
+    Not(And(hagrid, dumbledore)),
+    dumbledore
+)
+
+print(knowledge.formula())
